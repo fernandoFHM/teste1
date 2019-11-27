0e5d6437aca7d1bf4f9195d32b739028f9b7c1c4
leonardovere <leonardovere@hotmail.com>
2019-11-27 16:01:27 -0300

    Backup do banco MongoDB e projeto Backend em C#, também tem o protótipo

---
+++ Desenvolvimento_BackEnd/DevGames/DevGames/Controllers/GamesController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevGames.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Games/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // POST: Games/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Games/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Games/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Desenvolvimento_BackEnd/DevGames/DevGames/Controllers/Models/Games.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevGames.Controllers.Models
+{
+    public class Games
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        [BsonElement("Name")]
+        [Required]
+        public string GameName { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string[] Pataform { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
